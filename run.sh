d7fb0ef4217010de5cb00002a9a747d4e5f29d54
Filip Sikora <contact@fsikora.com>
2022-11-25 00:36:29 +0100

    use docker

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-podman run -it --rm --name=hyprland-build \
-	-- mount type=bind,source=${PWD},target=/root/src \
+docker run -it --rm --name=hyprland-build \
+	--mount type=bind,source=${PWD},target=/root/src \
 	tassilobalbo/hyprland-build:latest \
 	bash
